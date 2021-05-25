14a83893f4988bf36fbe948f30ec73ec121710e5
andres jaramillo <andres.jaramillo@ceiba.com.co>
2021-05-25 18:39:27 -0500

    initial commit

---
+++ parqueadero/parqueaderoApp.swift
@@ -0,0 +1,17 @@
+//
+//  parqueaderoApp.swift
+//  parqueadero
+//
+//  Created by Andrés Mauricio Jaramillo Romero - Ceiba Software on 25/05/21.
+//
+
+import SwiftUI
+
+@main
+struct parqueaderoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ parqueaderoTests/parqueaderoTests.swift
@@ -0,0 +1,22 @@
+//
+//  parqueaderoTests.swift
+//  parqueaderoTests
+//
+//  Created by Andrés Mauricio Jaramillo Romero - Ceiba Software on 25/05/21.
+//
+
+import XCTest
+
+class parqueaderoTests: XCTestCase {
+
+    
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssertTrue(true)
+    }
+
+    
+
+}
